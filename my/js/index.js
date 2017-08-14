5832775b4e1f9473b40fc2aaeca59a04b990df23
sjrams <apple@apple.local>
2017-08-14 09:08:40 +0800

    Revert "1.1"
    This reverts commit 25f364af54a55ade764c3483ecec14b2c6c2348b.

--- my/js/index.js
+++ my/js/index.js
@@ -3,7 +3,7 @@
 var tab;
 
 layui.config({
-	base: window.location.protocol + '//' + window.location.host + '/ci/my/js/',
+	base: 'http://' + window.location.host + '/ci/my/js/',
 	version:new Date().getTime()
 }).use(['element', 'layer', 'navbar', 'tab'], function() {
 	var element = layui.element(),
